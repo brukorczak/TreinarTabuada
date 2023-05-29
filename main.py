431de5ade7a5e0934874bac66a6f241ff41091ee
bruKorczak <bkorczak45@gmail.com>
2023-05-28 23:18:00 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,15 @@
+numero = int(input("Qual tabuada você quer treinar? Insira o número: "))
+
+acertos = 0
+erros = 0
+
+for n in range(1, 11):
+    resposta =  int(input(f"{numero} x {n} = ? "))
+    if resposta == (numero * n):
+        print("Correto!")
+        acertos = acertos + 1
+    else: print(f"Poxa, você errou, o valor correto é {numero * n}")
+    erros = erros + 1
+
+print(f"Total de acertos: {acertos}")
+print(f"Total de erros: {erros}")
